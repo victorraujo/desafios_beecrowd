42464f034f82d152eeb07573508bb23d7b056f36
victorraujo <victorrafaeldantas18@gmail.com>
2026-08-15 00:47:15 -0300

    iniciando desafio 1012

--- beecrowd/desafios/iniciante/1012_Área.c
+++ beecrowd/desafios/iniciante/1012_Área.c
@@ -1,6 +1,26 @@
+/*
+ ==========================================
+          ANOTAÇÕES DO PROJETO
+ ==========================================
+  
+  Autor:   Victor Rafael Dantas de Araujo
+  Data:    não finalizado
+  
+  Descrição:
+    descubra a área.
+    
+  Anotações importantes:
+    - ÄREAS A DESCUBRIR:
+    - descubra TRIANGULO
+    - descubra CIRCULO
+    - descubra TRAPEZIO
+    - descubra QUADRADO
+    - descubra RETANGULO
+ ==========================================
+ */
 #include <stdio.h>
 
 int main(void)
 {
-    
+
 }
