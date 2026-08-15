39ebdaffcc06b85954c710aa328af3d3c6dc09b3
victorraujo <victorrafaeldantas18@gmail.com>
2026-08-15 00:33:13 -0300

    corrigindo desafio 1011

--- beecrowd/desafios/iniciante/1011_Esfera.c
+++ beecrowd/desafios/iniciante/1011_Esfera.c
@@ -1,6 +1,29 @@
+/*
+ ==========================================
+          ANOTAÇÕES DO PROJETO
+ ==========================================
+  
+  Autor:   Victor Rafael Dantas de Araujo
+  Data:    13/08/2026
+  
+  Descrição:
+    calculando valor a pagar da peça.
+    
+  Anotações importantes:
+    - uma soma simples.
+    - compliquei um pouco usando ponteiro para treinar lógica.
+    - logica de ponteiros utilizando structs
+ ==========================================
+ */
 #include <stdio.h>
+#include <math.h>
+
+#define PI 3.14159 // valor de PI
 
 int main(void)
 {
-    
+    double raio, volume; // raio e oq vamos pegar e o resultado e volume
+
+    scanf("%lf", &raio);   // pegar o raio da bola
+
 }
