1fc09a9a1c0f6c2fcfbd59416699bbbdcd651ad8
victorraujo <victorrafaeldantas18@gmail.com>
2026-08-17 20:56:33 -0300

    finalizando desafio 1015

--- beecrowd/desafios/iniciante/1015.c
+++ beecrowd/desafios/iniciante/1015.c
@@ -4,15 +4,33 @@
  ==========================================
  
   Autor:   Victor Rafael Dantas de Araújo
-  Data:     não finalizado.
+  Data:     17/08/2026
  
   Descrição:
-    analisar a distancia entre pontos
+    Analisar a distancia entre pontos. basicamente a distancia entre 2 "personagens"
    
   Anotações importantes:
-    - 
+    - Aprendendo a usar a biblioteca math.h
     - 
     - 
 
  ==========================================
  */
+
+ 
+#include <stdio.h>
+#include <math.h> // sqrt(raiz), pow(potencia)
+
+int main(void)
+{
+    double x1,x2,y1,y2;
+
+    scanf ("%lf %lf %lf %lf", &x1, &y1, &x2, &y2);
+
+
+    //           FÓRMULA
+    double distancia = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+
+    printf("%.4lf\n", distancia);
+    return 0;
+}
