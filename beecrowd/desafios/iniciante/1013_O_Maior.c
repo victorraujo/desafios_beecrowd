fbd14429346a58afc2dd4ed4b57a87cd3525047d
victorraujo <victorrafaeldantas18@gmail.com>
2026-08-16 21:16:26 -0300

    iniciando desafio 1013

---
+++ beecrowd/desafios/iniciante/1013_O_Maior.c
@@ -0,0 +1,19 @@
+/*
+ ==========================================
+          ANOTAÇÕES DO PROJETO
+ ==========================================
+  
+  Autor:   Victor Rafael Dantas de Araujo
+  Data:    não finalizado.
+  
+  Descrição:
+    criar um programa pra dizer 
+    qual número e maior utilizando a fórmula.
+    
+  Anotações importantes:
+    - 
+    -
+    - 
+
+ ==========================================
+ */
