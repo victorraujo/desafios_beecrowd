b87a45b8622cbe6358c40b75c2e707935d29d8c9
victorraujo <victorrafaeldantas18@gmail.com>
2026-08-19 22:06:17 -0300

    resolvendo lógica matematica finalizada

--- beecrowd/desafios/iniciante/1018_Cédulas.c
+++ beecrowd/desafios/iniciante/1018_Cédulas.c
@@ -51,28 +51,42 @@ int main(void)
     zeramento(&dinheiro);
     int sobra = 0;
     int cedula = 0;
+    int valor_lido;
     
     scanf("%d", &cedula);
+    
+    
+    dinheiro.cem = cedula / 100;     //|  100
+    cedula = cedula % 100;           //| REAIS
 
-    dinheiro.cem = cedula / 100;
-    cedula = cedula % 100;
+    dinheiro.cinquenta = cedula / 50;               //|  50
+    cedula = cedula % 50;// a sobra da cedula % 100 //| REAIS
 
-    dinheiro.cinquenta = cedula / 50;
-    cedula = cedula % 50;
+    dinheiro.vinte = cedula / 20;    //|  20
+    cedula = cedula % 20;            //| REAIS
 
-    dinheiro.cem = cedula / 20;
-    cedula = cedula % 20;
+    dinheiro.dez = cedula / 10;      //|  10
+    cedula = cedula % 10;            //| REAIS
+
+    dinheiro.cinco = cedula / 5;     //|  5
+    cedula = cedula % 5;             //| REAIS
+
+    dinheiro.dois = cedula / 2;      //|  2
+    cedula = cedula % 2;             //| REAIS
+
+    dinheiro.um = cedula / 1;        //|  1
+    cedula = cedula % 1;             //| REAIS
 
 
-    // dinheiro.cem = cedula % sobra;
-
+    // display
+     printf("%d", valor_lido);
     printf("%d nota(s) de R$ 100,00\n", dinheiro.cem);
     printf("%d nota(s) de R$ 50,00\n", dinheiro.cinquenta);
     printf("%d nota(s) de R$ 20,00\n", dinheiro.vinte);
     printf("%d nota(s) de R$ 10,00\n", dinheiro.dez);
-    
+    printf("%d nota(s) de R$ 5,00\n", dinheiro.cinco);
+    printf("%d nota(s) de R$ 2,00\n", dinheiro.dois);
+    printf("%d nota(s) de R$ 1,00\n", dinheiro.um);
 
-
-
+    return 0;
 }
-
