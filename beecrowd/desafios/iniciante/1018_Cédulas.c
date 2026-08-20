1c69437185ad7cae8be601967b0946cc60af912c
victorraujo <victorrafaeldantas18@gmail.com>
2026-08-19 21:02:32 -0300

    inciando desafio 1018 cédulas em c

---
+++ beecrowd/desafios/iniciante/1018_Cédulas.c
@@ -0,0 +1,25 @@
+/*
+ ==========================================
+          ANOTAÇÕES DO PROJETO
+ ==========================================
+ 
+  Autor:   Victor Rafael Dantas de Araújo
+  Data:     não finalizado.
+ 
+  Descrição:
+    
+   
+  Anotações importantes:
+    - 
+    - 
+    - 
+
+ ==========================================
+ */
+
+#include <stdio.h>
+ 
+int main(void)
+{
+
+}
