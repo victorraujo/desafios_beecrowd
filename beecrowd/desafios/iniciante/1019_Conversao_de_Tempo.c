26c5522218439022529178845f725dacf7479d53
victorraujo <victorrafaeldantas18@gmail.com>
2026-08-20 23:13:41 -0300

    desafio 1019 iniciado

---
+++ beecrowd/desafios/iniciante/1019_Conversao_de_Tempo.c
@@ -0,0 +1,22 @@
+/*
+ ==========================================
+          ANOTAÇÕES DO PROJETO
+ ==========================================
+ 
+  Autor:   Victor Rafael Dantas de Araújo
+  Data:     não finalizado.
+ 
+  Descrição:
+    conversão de número para tempo(horas)
+   
+  Anotações importantes:
+    - digite o número e o programa vai converter em horas(horario)
+    - 
+    - 
+
+ ==========================================
+ */
+#include <stdio.h>
+
+
+int main(void)
