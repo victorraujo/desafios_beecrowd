72d8161a2cc4ad3cdea197e1af2a9988e82a0eea
victorraujo <victorrafaeldantas18@gmail.com>
2026-08-21 00:15:28 -0300

    finalizando desafio 1020

--- beecrowd/desafios/iniciante/1020_Idade_em_Dias.c
+++ beecrowd/desafios/iniciante/1020_Idade_em_Dias.c
@@ -4,13 +4,13 @@
 ==========================================
  
   Autor:   Victor Rafael Dantas de Araújo
-  Data:     não finalizado.
+  Data:     21\08\2026
  
   Descrição:
-    
+    contador de dias
    
   Anotações importantes:
-    - 
+    - diga um número e ele vai transformar em dias
     - 
     - 
 
@@ -23,4 +23,15 @@ int main(void)
     int usuario, ano, mes, dia;
 
     scanf("%d", &usuario);
+
+    ano = usuario / 365;
+    usuario = usuario % 365;
+
+    mes = usuario / 30;
+    usuario = usuario % 30;
+
+    dia = usuario;
+
+    printf("%d ano(s)\n%d mes(es)\n%d dia(s)\n", ano, mes, dia); // ano mes e dia
+    return 0;
 }
