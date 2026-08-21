4f6c9e53b9c7cf52f2f983b126158d78dbde79d4
victorraujo <victorrafaeldantas18@gmail.com>
2026-08-21 00:06:08 -0300

    iniciando desafio 1020

---
+++ beecrowd/desafios/iniciante/1020_Idade_em_Dias.c
@@ -0,0 +1,26 @@
+/*
+==========================================
+          ANOTAÇÕES DO PROJETO
+==========================================
+ 
+  Autor:   Victor Rafael Dantas de Araújo
+  Data:     não finalizado.
+ 
+  Descrição:
+    
+   
+  Anotações importantes:
+    - 
+    - 
+    - 
+
+==========================================
+*/
+#include <stdio.h>
+
+int main(void)
+{
+    int usuario, ano, mes, dia;
+
+    scanf("%d", &usuario);
+}
